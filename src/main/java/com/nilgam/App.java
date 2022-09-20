32b19996e21f72ae851a78da3badaa35d2892da8
Nilgam <nilgam@proton.me>
2022-09-20 21:24:09 +0200

    Init FXGL Project

---
+++ src/main/java/com/nilgam/App.java
@@ -0,0 +1,20 @@
+package com.nilgam;
+
+import com.almasb.fxgl.app.GameApplication;
+import com.almasb.fxgl.app.GameSettings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends GameApplication {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    protected void initSettings(GameSettings settings) {
+        // TODO Auto-generated method stub
+
+    }
+}
